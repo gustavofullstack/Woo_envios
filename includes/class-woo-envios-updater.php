5555d52af3b19f750fa7470d14b532091e7cf272
acmf-gov <acmf_gov@protonmail.com>
2025-12-19 10:02:46 -0300

    feat: Migrate plugin auto-update mechanism from GitHub API to a `plugin-update.json` file, and add workflow to manage it.

--- includes/class-woo-envios-updater.php
+++ includes/class-woo-envios-updater.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Class Woo_Envios_Updater
  *
- * Handles automatic updates from GitHub.
+ * Handles automatic updates via plugin-update.json.
  */
 class Woo_Envios_Updater {
 
@@ -15,8 +15,7 @@ class Woo_Envios_Updater {
 	private $username;
 	private $repo;
 	private $plugin_file;
-	private $github_api_result;
-	private $access_token;
+	private $json_url;
 
 	/**
 	 * Class constructor.
@@ -24,91 +23,66 @@ class Woo_Envios_Updater {
 	 * @param string $plugin_file Absolute path to the main plugin file.
 	 * @param string $username    GitHub username.
 	 * @param string $repo        GitHub repository name.
-	 * @param string $access_token Optional GitHub Private Access Token.
 	 */
-	public function __construct( $plugin_file, $username, $repo, $access_token = '' ) {
-		$this->plugin_file  = $plugin_file;
-		$this->username     = $username;
-		$this->repo         = $repo;
-		$this->access_token = $access_token;
-		$this->slug         = plugin_basename( $this->plugin_file );
+	public function __construct( $plugin_file, $username, $repo ) {
+		$this->plugin_file = $plugin_file;
+		$this->username    = $username;
+		$this->repo        = $repo;
+		$this->slug        = plugin_basename( $this->plugin_file );
+		
+		// Fonte da verdade: JSON na raiz do repo (Raw)
+		$this->json_url = "https://raw.githubusercontent.com/{$this->username}/{$this->repo}/main/plugin-update.json";
 
-		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'set_transient' ) );
-		add_filter( 'plugins_api', array( $this, 'set_plugin_info' ), 10, 3 );
-		add_filter( 'upgrader_post_install', array( $this, 'post_install' ), 10, 3 );
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'check_update' ) );
+		add_filter( 'plugins_api', array( $this, 'check_info' ), 10, 3 );
+		add_filter( 'upgrader_process_complete', array( $this, 'track_update' ), 10, 2 );
 	}
 
 	/**
-	 * Get the latest release from GitHub.
-	 *
-	 * @return object|bool JSON decoded object or false on failure.
+	 * Retrieve update info from the remote JSON file.
+	 * 
+	 * @return object|bool
 	 */
-	private function get_github_release_info() {
-		if ( ! empty( $this->github_api_result ) ) {
-			return $this->github_api_result;
-		}
+	private function get_remote_info() {
+		$request = wp_remote_get( $this->json_url );
 
-		$url = "https://api.github.com/repos/{$this->username}/{$this->repo}/releases/latest";
-
-		// Add Access Token if provided
-		$args = array();
-		if ( ! empty( $this->access_token ) ) {
-			$args['headers'] = array(
-				'Authorization' => "token {$this->access_token}",
-			);
-		}
-
-		$request = wp_remote_get( $url, $args );
-
-		if ( is_wp_error( $request ) ) {
+		if ( is_wp_error( $request ) || 200 !== wp_remote_retrieve_response_code( $request ) ) {
 			return false;
 		}
 
 		$body = wp_remote_retrieve_body( $request );
 		$data = json_decode( $body );
 
-		if ( ! empty( $data ) && is_object( $data ) && isset( $data->tag_name ) ) {
-			$this->github_api_result = $data;
-			return $data;
+		if ( empty( $data ) || ! is_object( $data ) ) {
+			return false;
 		}
 
-		return false;
+		return $data;
 	}
 
 	/**
-	 * Check for updates and set the transient.
-	 *
-	 * @param object $transient The update transient.
-	 * @return object
+	 * Push update to the transient.
 	 */
-	public function set_transient( $transient ) {
+	public function check_update( $transient ) {
 		if ( empty( $transient->checked ) ) {
 			return $transient;
 		}
 
-		// Get current version
-		$this->plugin_data = get_plugin_data( $this->plugin_file );
-		$current_version   = $this->plugin_data['Version'];
-
-		// Get remote info
-		$remote_info = $this->get_github_release_info();
-
-		if ( ! $remote_info ) {
+		$remote = $this->get_remote_info();
+		if ( ! $remote ) {
 			return $transient;
 		}
 
-		// Check if remote version is newer
-		// Clean v prefix if exists. Ex: v1.0.0 -> 1.0.0
-		$remote_version = str_replace( 'v', '', $remote_info->tag_name );
+		$this->plugin_data = get_plugin_data( $this->plugin_file );
+		$current_version   = $this->plugin_data['Version'];
 
-		if ( version_compare( $current_version, $remote_version, '<' ) ) {
-			$obj              = new stdClass();
+		if ( version_compare( $current_version, $remote->version, '<' ) ) {
+			$obj = new stdClass();
 			$obj->slug        = $this->slug;
-			$obj->new_version = $remote_version;
-			$obj->url         = $remote_info->html_url;
-			$obj->package     = $remote_info->zipball_url;
+			$obj->new_version = $remote->version;
+			$obj->url         = $this->json_url;
+			$obj->package     = $remote->download_url;
 
-			// Add to transient
 			$transient->response[ $this->slug ] = $obj;
 		}
 
@@ -116,59 +90,47 @@ class Woo_Envios_Updater {
 	}
 
 	/**
-	 * Display plugin details in the "View Details" popup.
-	 *
-	 * @param bool   $result The current result.
-	 * @param string $action The action being performed.
-	 * @param object $args   The arguments.
-	 * @return object|bool
+	 * Push info to the "View Details" popup.
 	 */
-	public function set_plugin_info( $result, $action, $args ) {
+	public function check_info( $res, $action, $args ) {
 		if ( 'plugin_information' !== $action ) {
-			return $result;
+			return $res;
 		}
 
 		if ( empty( $args->slug ) || $this->slug !== $args->slug ) {
-			return $result;
+			return $res;
 		}
 
-		$remote_info = $this->get_github_release_info();
-
-		if ( ! $remote_info ) {
-			return $result;
+		$remote = $this->get_remote_info();
+		if ( ! $remote ) {
+			return $res;
 		}
 
-		$plugin_data = get_plugin_data( $this->plugin_file );
+		$res = new stdClass();
+		$res->name           = $remote->name;
+		$res->slug           = $this->slug;
+		$res->version        = $remote->version;
+		$res->tested         = $remote->tested;
+		$res->requires       = $remote->requires;
+		$res->author         = $remote->author;
+		$res->author_profile = $remote->author_profile;
+		$res->download_link  = $remote->download_url;
+		$res->trunk          = $remote->download_url;
+		$res->requires_php   = $remote->requires_php;
+		$res->last_updated   = $remote->last_updated;
+		$res->sections       = (array) $remote->sections;
 
-		$obj                = new stdClass();
-		$obj->name          = $plugin_data['Name'];
-		$obj->slug          = $this->slug;
-		$obj->version       = str_replace( 'v', '', $remote_info->tag_name );
-		$obj->author        = $plugin_data['AuthorName'];
-		$obj->homepage      = $remote_info->html_url;
-		$obj->requires      = '5.0'; // Default requirement
-		$obj->tested        = get_bloginfo( 'version' ); // Tested up to current version
-		$obj->downloaded    = 0;
-		$obj->last_updated  = $remote_info->published_at;
-		$obj->sections      = array(
-			'description' => $remote_info->body, // Use release notes as description
-		);
-		$obj->download_link = $remote_info->zipball_url;
+		if ( ! empty( $remote->banners ) ) {
+			$res->banners = (array) $remote->banners;
+		}
 
-		return $obj;
+		return $res;
 	}
 
 	/**
-	 * Post install actions.
-	 *
-	 * @param bool   $true       True.
-	 * @param array  $hook_extra Extra args.
-	 * @param object $result     Result object.
-	 * @return bool
+	 * Opcional: Track update (simulado).
 	 */
-	public function post_install( $true, $hook_extra, $result ) {
-		// Can be used to rename folder if GitHub extracted it with a weird name
-		// For now simple return true
-		return $true;
+	public function track_update( $upgrader_object, $options ) {
+		// Logica de tracking poderia vir aqui
 	}
 }
